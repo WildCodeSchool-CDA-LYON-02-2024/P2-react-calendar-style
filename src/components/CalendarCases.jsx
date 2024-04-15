4bab77ae776293e020873b140a3e8b2fe0c4b4ee
Patrick Hoang <hoang.patrick@outlook.com>
2024-04-15 15:49:24 +0200

    modification month

--- src/components/CalendarCases.jsx
+++ src/components/CalendarCases.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import "./CalendarCases.css";
 
 export default function CalendarCases() {
-  const [mode, setMode] = useState("mois"); // 'mois' par défaut
+  const [mode, setMode] = useState("month");
   const [selectedMonth, setSelectedMonth] = useState(null);
 
   const yearFr = {
-    mois: [
+    month: [
       { name: "Janvier", nbDays: 31 },
-      { name: "Février", nbDays: 29 }, // Année bissextile
+      { name: "Février", nbDays: 29 },
       { name: "Mars", nbDays: 31 },
       { name: "Avril", nbDays: 30 },
       { name: "Mai", nbDays: 31 },
@@ -23,21 +23,21 @@ export default function CalendarCases() {
   };
 
   const handleMonthClick = (index) => {
-    setSelectedMonth(yearFr.mois[index]);
+    setSelectedMonth(yearFr.month[index]);
     setMode("nbDays");
   };
 
   return (
     <div className="calendarCasesContainer">
       <div>
-        <button onClick={() => setMode("mois")}>Afficher les mois</button>
+        <button onClick={() => setMode("month")}>Afficher les mois</button>
       </div>
-      {mode === "mois" && (
+      {mode === "month" && (
         <div className="allMonthsContainer">
-          {yearFr.mois.map((mois, index) => (
+          {yearFr.month.map((month, index) => (
             <div key={index} className="monthsList">
               <button onClick={() => handleMonthClick(index)}>
-                {mois.name}
+                {month.name}
               </button>
             </div>
           ))}
