33ec4c46ed5f6ab54f0eb41a08b7ae99d5e2ba60
Patrick Hoang <hoang.patrick@outlook.com>
2024-04-29 10:53:55 +0200

    modification organisation compostants

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import SaisonImg from "./components/saison_img/SaisonImg";
+import InputDate from "./components/inputs/InputDate";
+
+import WeekDaysCases from "./components/weekDaysCase/WeekDaysCases.jsx";
+import CalendarCases from "./components/calendarCases/CalendarCases";
+
+import Grille from "./components/grille/Grille";
+import DarkModeToggle from "./components/darkMode/DarkModeToggle.jsx";
+
+export {
+  SaisonImg,
+  InputDate,
+  WeekDaysCases,
+  CalendarCases,
+  Grille,
+  DarkModeToggle,
+};
